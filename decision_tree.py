a882f03947c7c1022413da38c291b67de3873a18
Romain <romain.storai@rstr.fr>
2023-03-17 16:00:55 +0000

    added new models

---
+++ decision_tree.py
@@ -0,0 +1,39 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+
+from experiment_baseplate import load_split_data, get_text_data
+
+X_train, y_train, X_validate, y_validate, X_test, y_test = load_split_data()
+
+#Text processing
+MAX_INPUT_LENGTH = 1000
+
+tockenizer = CountVectorizer(analyzer="word")
+tockenizer.fit(get_text_data())
+
+def tockenize(data):
+    n_data = tockenizer.transform(data)
+    # if(len(n_data)>MAX_INPUT_LENGTH):
+    #     ndata = ndata[:MAX_INPUT_LENGTH]
+    # else:
+    return n_data
+        
+
+# Create a decision tree classifier
+clf = DecisionTreeClassifier()
+
+# Train the classifier on the training data
+print("Training...")
+clf.fit(tockenize(X_train), y_train)
+print("Training finished...")
+
+# Predict the class labels for the testing data
+#y_pred = clf.predict(tockenize(X_text))
+
+# Evaluate the accuracy of the classifier
+accuracy = clf.score(tockenize(X_validate), y_validate)
+
+# Print the accuracy
+print("Accuracy:", accuracy)
+
+print(clf.predict(tockenize(["You are shit", "You are in the shit", "You are an eye sore", "I don't like you", "I enjoy your company", "You are biatch", "You biiitch"])))
--- experiment_baseplate.py
+++ experiment_baseplate.py
@@ -1,8 +1,11 @@
+'''This modules contains the baseplate methods to load train, test and validation data.
+'''
+
 import pandas as pd
 import numpy as np
 
 
-def prepare_data(dataframe: pd.DataFrame):
+def _prepare_data(dataframe: pd.DataFrame):
     '''Split the dataframe in inputs and labels.
     The labels are divided in two columns, corresponding to each class
     '''
@@ -12,6 +15,14 @@ def prepare_data(dataframe: pd.DataFrame):
     return (np.array(dataframe["text"]), np.array(dataframe[["appro", "inappro"]]))
 
 
-X_train, y_train = prepare_data(pd.read_csv("data/train.csv"))
-X_validate, y_validate = prepare_data(pd.read_csv("data/validate.csv"))
-X_test, y_test = prepare_data(pd.read_csv("data/test.csv"))
+def load_split_data():
+    '''Load the full set of data splitted into train, test and validation
+    '''
+    X_train, y_train = _prepare_data(pd.read_csv("data/train.csv"))
+    X_validate, y_validate = _prepare_data(pd.read_csv("data/validate.csv"))
+    X_test, y_test = _prepare_data(pd.read_csv("data/test.csv"))
+    return (X_train, y_train, X_validate, y_validate, X_test, y_test)
+
+
+def get_text_data():
+    return pd.read_csv("data/full.csv")["text"]
